67c817504d9829b614f49d0b0159fd6c8ee0208d
reddragonnm <neeleshojmishra@gmail.com>
2025-10-02 00:55:07 +0530

    Added tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1>The Trust Gambit</h1>
+      <h1 className="text-4xl font-bold">The Trust Gambit</h1>
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "./main.css";
+
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
