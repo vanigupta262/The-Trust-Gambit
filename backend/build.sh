bfb3de3fb21fabf0554b68cb08c3dd635eadd70a
Mridul Chhipa <mridulchhipa@gmail.com>
2025-10-11 02:11:25 +0530

    hiii

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
-pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic
-python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate --no-input
